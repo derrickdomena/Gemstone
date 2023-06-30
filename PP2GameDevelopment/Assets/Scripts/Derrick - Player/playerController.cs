11d3775e5c37419515da87cd76ade2048b557447
Derrick Domena <derrickdomenawork@gmail.com>
2023-06-30 15:43:21 -0400

    Created Player.
    Created 3D Object capsule called Player, and wrote a C# script for player movement called playerController.
    
    Added subfolders to Assets to add distinction of my test Materials and Prefabs.

---
+++ PP2GameDevelopment/Assets/Scripts/Derrick - Player/playerController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+
+    [SerializeField] float playerSpeed;
+    [SerializeField] float jumpHeight;
+    [SerializeField] float gravityValue;
+    [SerializeField] int jumpsMax;
+
+    Vector3 move;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    int jumpCount;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+            jumpCount = 0;
+        }
+
+        move = (transform.right * Input.GetAxis("Horizontal")) +
+               (transform.forward * Input.GetAxis("Vertical"));
+
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpsMax)
+        {
+            playerVelocity.y = jumpHeight;
+            jumpCount++;
+        }
+
+        playerVelocity.y -= gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
