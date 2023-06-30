97bd134cf6c8bae24c7aa18f39c637739cd0f103
EyonNorman <Ejnorman@student.fullsail.edu>
2023-06-30 15:24:44 -0500

    UI(crosshair, GameManager,ButtonFunctions,Pause screen)
    Got the Crosshair dot in the middle of the screen. Got the Pause menu working when pressing Escape. Game manager handles button presses. ButtonFunctions handle button logic

---
+++ PP2GameDevelopment/Assets/Scripts/gameManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    public static gameManager instance;
+    [Header("----- Player Stuff -----")]
+    public GameObject player;
+
+
+    [Header("----- UI Stuff -----")]
+    public GameObject activeMenu;
+    public GameObject pauseMenu;
+    public GameObject winMenu;
+    public GameObject loseMenu;
+
+
+
+    bool isPaused;
+    float timescaleOrig;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+        player = GameObject.FindGameObjectWithTag("Player");
+        timescaleOrig = Time.timeScale; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Cancel") && activeMenu == null) 
+        {
+            statePaused();
+            activeMenu = pauseMenu;
+            activeMenu.SetActive(isPaused);
+        }
+    }
+    public void statePaused()
+    {
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+        isPaused = !isPaused;
+    }
+    public void stateUnpaused()
+    {
+        Time.timeScale = timescaleOrig;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        isPaused = !isPaused;
+        activeMenu.SetActive(false);
+        activeMenu = null;
+    }
+}
