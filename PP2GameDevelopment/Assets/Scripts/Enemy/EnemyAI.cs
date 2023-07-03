4040492358ce78ca0a48096508be02b98ad454d5
Kishgla <Kishgla@gmail.com>
2023-07-03 10:51:49 -0700

    Enemy updates game goal

--- PP2GameDevelopment/Assets/Scripts/Enemy/EnemyAI.cs
+++ PP2GameDevelopment/Assets/Scripts/Enemy/EnemyAI.cs
@@ -6,20 +6,39 @@ public class EnemyAI : MonoBehaviour, IDamage
 {
     [Header("----- Components -----")]
     [SerializeField] Renderer model;
+    [SerializeField] Transform headPos;
 
     [Header("----- Stats -----")]
     [SerializeField] int hp;
+    [SerializeField] int moveSpeed;
+
+    bool playerInRange;
+    Vector3 playerDir;
+    float angleToPlayer;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager.instance.updateGameGoal(1);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void facePlayer()
+    {
+        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, 0, playerDir.z));
+    }
+
+    bool canSeePlayer()
+    {
+        playerDir = gameManager.instance.player.transform.position - headPos.position;
+        angleToPlayer = Vector3.Angle(new Vector3(playerDir.x, 0, playerDir.z), transform.forward);
+        
+        return false;
     }
 
     public void TakeDamage(int damage)
