708c8542e36aa026a3856337af4abe195f7b19a6
Kishgla <Kishgla@gmail.com>
2023-07-03 14:44:05 -0700

    Update/merge from main

--- PP2GameDevelopment/Assets/Scripts/Enemy/EnemyAI.cs
+++ PP2GameDevelopment/Assets/Scripts/Enemy/EnemyAI.cs
@@ -1,15 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour, IDamage
 {
     [Header("----- Components -----")]
     [SerializeField] Renderer model;
+    [SerializeField] Transform headPos;
+    [SerializeField] NavMeshAgent agent;
 
     [Header("----- Stats -----")]
-    [SerializeField] int hp;
-    
+    [Range(1, 50)][SerializeField] int hp;
+    [Range(1, 20)][SerializeField] int moveSpeed;
+    [Range(1, 180)][SerializeField] int viewAngle;
+    [Range(1, 10)][SerializeField] int playerFaceSpeed;
+
+    [Header("----- Gun stuff -----")]
+    [SerializeField] float shootRate;
+    [SerializeField] Transform shootPos;
+    [SerializeField] GameObject bullet;
+
+    bool playerInRange;
+    Vector3 playerDir;
+    float angleToPlayer;
+    bool isShooting;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +36,49 @@ public class EnemyAI : MonoBehaviour, IDamage
     void Update()
     {
         
+    }
+
+    void FacePlayer()
+    {
+        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, 0, playerDir.z));
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * playerFaceSpeed);
+    }
+
+    bool CanSeePlayer()
+    {
+        playerDir = gameManager.instance.player.transform.position - headPos.position;
+        angleToPlayer = Vector3.Angle(new Vector3(playerDir.x, 0, playerDir.z), transform.forward);
+
+        RaycastHit hit;
+        if (Physics.Raycast(headPos.position, playerDir, out hit))
+        {
+            if (hit.collider.CompareTag("Player") && angleToPlayer < viewAngle)
+            {
+                agent.SetDestination(gameManager.instance.player.transform.position);
+
+                if (agent.remainingDistance < agent.stoppingDistance)
+                {
+                    FacePlayer();
+                }
+
+                if (!isShooting)
+                {
+                    StartCoroutine(Shoot());
+                }
+            }
+        }
+        
+        return false;
+    }
+
+    IEnumerator Shoot()
+    {
+        isShooting = true;
+
+        Instantiate(bullet, shootPos.position, transform.rotation);
+
+        yield return new WaitForSeconds(shootRate);
+        isShooting = false;
     }
 
     public void TakeDamage(int damage)
