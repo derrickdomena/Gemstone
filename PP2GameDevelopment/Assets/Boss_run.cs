2f71ede84544e3dc45875414783e19dadad03c53
EyonNorman <Ejnorman@student.fullsail.edu>
2023-07-17 12:38:50 -0500

    F

---
+++ PP2GameDevelopment/Assets/Boss_run.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+
+public class Boss_run : StateMachineBehaviour
+{
+    //public float moveSpeed = 2.5f;
+    //Transform player;
+    
+    
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    player =  GameObject.FindGameObjectWithTag("Player").transform;
+    //     
+    //}
+    //
+    //// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    Vector3 target = new Vector3(player.position.x, , player.position.z);
+    //    Vector3 newPos = Vector3.MoveTowards(, target, moveSpeed * Time.fixedDeltaTime);
+    //    
+    //}
+    //
+    //// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+
+}
