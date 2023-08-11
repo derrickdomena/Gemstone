89e49716eb44d10022bcb57faf1f5d059ec27acf
Derrick Domena <derrickdomenawork@gmail.com>
2023-08-11 15:53:15 -0400

    Added Level Loader Functionality
    Level Loader will handle the transition between scenes. Loads a level asynchronously while showing a progress bar.

---
+++ PP2GameDevelopment/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private Slider loadingSlider;
+
+    private void Start()
+    {
+        loadingScreen = GameObject.Find("LoadingScreen");
+        mainMenu = GameObject.Find("MainMenu");
+        loadingSlider = GameObject.Find("LoadingScreen/LoadingSlider").GetComponent<Slider>();
+        loadingScreen.SetActive(false);
+    }
+
+    public void LoadLevel(int level)
+    {
+        mainMenu.SetActive(false);
+        loadingScreen.SetActive(true);
+
+        StartCoroutine(LoadLevelASync(level));
+    }
+
+    IEnumerator LoadLevelASync(int level)
+    {
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(level);
+
+        while (!loadOperation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(loadOperation.progress / 0.9f);
+            loadingSlider.value = progressValue;
+            yield return null;
+        }
+    }
+}
