0d87958538e53b25a2acefe1bef9505535e75455
Derrick Domena <derrickdomenawork@gmail.com>
2023-08-11 04:34:20 -0400

    Added Fireball Ability Functionality
    Fireball Ability shoots a fiery projectile that explodes on impact.
    
    This ability replaced the grenade.
    
    This update contains:
    Added Fireball prefab.
    
    Added Fireball script, handles the particle effect, blast radius, blast force, and blast damage.
    
    Added Fireball Ability script to player prefab, handles the fireball projectile object, fireball position, and fireball speed.

---
+++ PP2GameDevelopment/Assets/Scripts/Abilities/Fireball.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    [Header("----- Fireball Components -----")]
+    [SerializeField] GameObject explosionEffect;
+
+    [Header("----- Fireball Stats -----")]
+    [SerializeField] float blastRadius;
+    [SerializeField] float blastForce;
+    [SerializeField] int blastDamage;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Explode();
+    }
+
+    void Explode()
+    {
+        // Particle Effect
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+
+        // Add force to nearby objects
+        Collider[] colliders = Physics.OverlapSphere(transform.position, blastRadius);
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            // Add force to rigidbody objects
+            Rigidbody rb = colliders[i].GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(blastForce, transform.position, blastRadius);
+            }
+
+            // Damage objects that inherit IDamage
+            IDamage damageable = colliders[i].GetComponent<IDamage>();
+            if (damageable != null && !colliders[i].CompareTag("Player"))
+            {
+                damageable.TakeDamage(blastDamage);
+            }
+        }
+
+        // Remove object
+        Destroy(gameObject);
+    }
+}
---
+++ PP2GameDevelopment/Assets/Scripts/Abilities/FireballAbility.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballAbility : MonoBehaviour
+{
+    [Header("----- Fireball Components -----")]
+    public GameObject fireballProjectile;
+    public Transform fireballPosition;
+
+    [Header("----- Fireball Stats -----")]
+    public float fireballSpeed;
+
+    KeyCode fireballKey = KeyCode.Q;
+    bool canThrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager.instance.grenadeCooldownFill.fillAmount = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateGrenadeUI();
+    }
+
+    void ThrowFireball()
+    {
+        canThrow = false;
+
+        // Instantiae fireball object
+        GameObject fireballObj = Instantiate(fireballProjectile, fireballPosition.transform.position, fireballPosition.transform.rotation);
+
+        // Get rigidbody component from fireball
+        Rigidbody rb = fireballObj.GetComponent<Rigidbody>();
+
+        // Add throwing force
+        Vector3 forwardForce = transform.Find("Main Camera").forward * fireballSpeed;
+        rb.AddForce(forwardForce, ForceMode.Impulse);
+    }
+
+    // Updates the Fireball UI
+    void UpdateGrenadeUI()
+    {
+        // When grenadeKey is pressed and canThrow is true, you can throw a grenade
+        // also checks to see if the game is paused
+        if (Input.GetKeyDown(fireballKey) && !canThrow && Time.timeScale != 0)
+        {
+            //change function to fireballCooldown
+            gameManager.instance.grenadeCooldownFill.fillAmount = 0;
+            ThrowFireball();
+            canThrow = true;
+        }
+
+        // When canThrow is true, start incrementing the grenade ability image fill amount
+        if (canThrow)
+        {
+            //change function to fireballCooldown
+            gameManager.instance.grenadeCooldownFill.fillAmount += 1 / gameManager.instance.playerScript.grenadeCooldown * Time.deltaTime;
+
+            // When image fill amount is equal to one, set canThrow to false
+            //change function to fireballCooldown
+            if (gameManager.instance.grenadeCooldownFill.fillAmount == 1)
+            {
+                canThrow = false;
+            }
+        }
+    }
+
+    public void UpdateCooldownGrenade(float time)
+    {
+        //change function to fireballCooldown
+        gameManager.instance.playerScript.grenadeCooldown = gameManager.instance.playerScript.grenadeCooldown - time;
+        canThrow = true;
+        UpdateGrenadeUI();
+    }
+}
