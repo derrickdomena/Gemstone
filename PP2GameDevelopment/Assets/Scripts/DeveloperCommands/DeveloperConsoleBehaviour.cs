e9c68efb8fc37984bad76768e4605d433b165134
EyonNorman <Ejnorman@student.fullsail.edu>
2023-08-15 16:34:19 -0500

    Dev console Ui
    Added Ui when dev console is added that shows the player commands that they can use. also added the dev console to Hub scene and boss scene

--- PP2GameDevelopment/Assets/Scripts/DeveloperCommands/DeveloperConsoleBehaviour.cs
+++ PP2GameDevelopment/Assets/Scripts/DeveloperCommands/DeveloperConsoleBehaviour.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using static UnityEngine.InputSystem.InputAction;
+
+
 public class DeveloperConsoleBehaviour : MonoBehaviour
 {
     [SerializeField] private string prefix = string.Empty;
     [SerializeField] private ConsoleCommand[] commands = new ConsoleCommand[0];
-
     [Header("UI")]
     [SerializeField] private GameObject uiCanvas = null;
     [SerializeField] private TMP_InputField inputField = null;
