a100d688b12faf909c77ab3bd75adc89ee700aa8
Will Ford <williamford83@gmail.com>
2023-08-24 10:45:14 -0500

    Pitfall fix and various missing colliders added

--- PP2GameDevelopment/Assets/Scripts/Traps/DeathBarrier.cs
+++ PP2GameDevelopment/Assets/Scripts/Traps/DeathBarrier.cs
@@ -5,20 +5,11 @@ using UnityEngine;
 
 public class DeathBarrier : MonoBehaviour
 {
-
-    public void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            gameManager.instance.anim.SetTrigger("FadeOut");
-        }
-    }
-
     public void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            gameManager.instance.playerScript.TakeDamage(gameManager.instance.playerScript.hp);
+            gameManager.instance.youLose();
         }
     }
 }
